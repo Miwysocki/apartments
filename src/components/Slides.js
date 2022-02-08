8f8ab82d0fa8f64266bdbeddc1d3db2789f538dc
Miwysocki <34211963+Miwysocki@users.noreply.github.com>
2022-02-08 09:36:55 +0100

    explore nerby

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { useNavigate } from "react-router-dom";
 import "../style/Slides.css";
 
 const responsive = {
@@ -28,6 +30,76 @@ const responsive = {
 </Carousel>;
 
 const Slides = () => {
+  const {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+  } = require("react-google-maps");
+  const navigate = useNavigate();
+  const [currentPosition, setCurrentPosition] = useState();
+  const [city, setCity] = useState();
+  const geocoder = new window.google.maps.Geocoder();
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  function getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }
+
+  function showPosition(position) {
+    setCurrentPosition(position);
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    codeLatLng(lat, lng);
+  }
+
+  async function handleExploreNerby() {
+    if (currentPosition) {
+      const lat = currentPosition.coords.latitude;
+      const lng = currentPosition.coords.longitude;
+
+      navigate("/search/" + city);
+    }
+  }
+  function codeLatLng(lat, lng) {
+    let city = "";
+
+    var latlng = new window.google.maps.LatLng(lat, lng);
+
+    const g = geocoder.geocode({ latLng: latlng }, function (results, status) {
+      if (status == window.google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+          for (var i = 0; i < results[0].address_components.length; i++) {
+            for (
+              var b = 0;
+              b < results[0].address_components[i].types.length;
+              b++
+            ) {
+              if (
+                results[0].address_components[i].types[b] ==
+                "administrative_area_level_2"
+              ) {
+                city = results[0].address_components[i].long_name;
+                setCity(city);
+                break;
+              }
+            }
+          }
+        } else {
+          alert("No results found");
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  }
+
   return (
     <div>
       <Carousel
@@ -46,7 +118,7 @@ const Slides = () => {
           <p>Unique Places</p>
         </div>
 
-        <div className="tile">
+        <div className="tile" onClick={handleExploreNerby}>
           {" "}
           <img
             src="https://images.pexels.com/photos/1252500/pexels-photo-1252500.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,24 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "../Header";
 import Banner from "../Banner";
 import Home from "../Home";
 import Footer from "../components/Footer";
 
 const Homepage = () => {
-  useEffect(() => {
-    getLocation();
-  }, []);
-
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  }
-  function showPosition(position) {
-    console.log("position:", position);
-  }
   return (
     <div>
       <Header />
